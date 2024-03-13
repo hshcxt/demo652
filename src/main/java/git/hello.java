e48ac93c1b9c56bfeb09fd858bb5c1b07cf9b3c2
hsh <1625292672@qq.com>
2024-03-13 16:36:05 +0800

    three

--- src/main/java/git/hello.java
+++ src/main/java/git/hello.java
@@ -4,5 +4,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("你是猪吗 啊发撒");
         System.out.println("的急急急");
+        System.out.println("你是猪吗 啊发方法撒");
+
     }
 }
